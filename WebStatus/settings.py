860336f62bdbebf20e110e461faf380ca9e066e2
Alex Rasmussen <alexras@acm.org>
2011-01-15 14:37:10 -0800

    Adding a template for the index page of the status app.

--- WebStatus/settings.py
+++ WebStatus/settings.py
@@ -81,6 +81,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/alexras/src/cse-arcade/WebStatus/templates",
 )
 
 INSTALLED_APPS = (
