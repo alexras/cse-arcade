9d67cc033d9f87ff99e0f969c67f013305a4f798
Alex Rasmussen <alexras@acm.org>
2011-01-17 23:53:00 -0800

    Make view load based on $HOME/status_updates/status_update JSON file.

--- WebStatus/status/views.py
+++ WebStatus/status/views.py
@@ -2,25 +2,43 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from status.models import Platform, Game
+from django.conf import settings
+import json
+import os
+
+class Game(object):
+    def __init__(self, name, time=None, plays=None):
+        self.name = name
+        self.time = time
+        self.plays = plays
 
 def index(request):
-    platform_list = Platform.objects.all().order_by('name')
+    STATUS_UPDATE_FILE = "%s/status_updates/status_update" % (os.getenv("HOME"))
+    assert(os.path.exists(STATUS_UPDATE_FILE))
+    fp = open(STATUS_UPDATE_FILE, 'r')
+    status = json.load(fp)
+    fp.close()
     
-    top_ten_by_total_time = Game.objects.all().order_by('-total_time')[:10]
-    top_ten_by_play_count = Game.objects.all().order_by('-plays')[:10]
+    postprocessed_status = {}
     
-    total_play_time_seconds = sum([game.total_time for game in \
-                                       Game.objects.all()])
-
-    total_play_time = "%d days, %d hours, %02d minutes, %02d seconds" % \
-        ((total_play_time_seconds / 86400,
-          (total_play_time_seconds / 3600) % 24, 
-          (total_play_time_seconds / 60) % 60, 
-          total_play_time_seconds % 60))
-         
-    return render_to_response("status/index.html", 
-                              {"platform_list" : platform_list,
-                               "top_ten_by_total_time" : top_ten_by_total_time,
-                               "top_ten_by_play_count" : top_ten_by_play_count,
-                               "total_play_time" : total_play_time})
+    postprocessed_status['playing'] = bool(status['status'])
+    postprocessed_status['current_game'] = status['current_game']
+    
+    top_ten_by_total_time = []
+    top_ten_by_plays = []
+    
+    for i in xrange(10):
+        time_info = status['times'][str(i + 1)]
+        plays_info = status['plays'][str(i + 1)]
+        
+        time_game = Game(time_info[0], time=time_info[1])
+        plays_game = Game(plays_info[0], plays=plays_info[1])
+        
+        top_ten_by_total_time.append(time_game)
+        top_ten_by_plays.append(plays_game)
+    
+    postprocessed_status['top_ten_by_total_time'] = top_ten_by_total_time
+    postprocessed_status['top_ten_by_plays'] = top_ten_by_plays
+    postprocessed_status['total_time'] = status['total_time']
+    
+    return render_to_response("status/index.html",postprocessed_status)
