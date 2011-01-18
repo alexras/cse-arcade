6aa7cab1cee704edd1b09eaeee4007427f751e56
Alex Rasmussen <alexras@acm.org>
2011-01-18 11:49:01 -0800

    Convert times from seconds, add last_updated field.

--- WebStatus/status/views.py
+++ WebStatus/status/views.py
@@ -5,12 +5,18 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 import json
 import os
+import datetime
+import time
 
 class Game(object):
     def __init__(self, name, time=None, plays=None):
         self.name = name
         self.time = time
         self.plays = plays
+
+def readable_time(seconds):
+    td = datetime.timedelta(seconds=seconds)
+    return str(td)
 
 def index(request):
     STATUS_UPDATE_FILE = "/home/arcade/status_updates/status_update"
@@ -25,6 +31,8 @@ def index(request):
     postprocessed_status['playing'] = bool(status['status'])
     postprocessed_status['current_game'] = status['current_game']
     
+    postprocessed_status['last_update'] = time.ctime(int(status['last_update']))
+    
     top_ten_by_total_time = []
     top_ten_by_plays = []
     
@@ -32,7 +40,7 @@ def index(request):
         time_info = status['times'][str(i + 1)]
         plays_info = status['plays'][str(i + 1)]
         
-        time_game = Game(time_info[0], time=time_info[1])
+        time_game = Game(time_info[0], time=readable_time(time_info[1]))
         plays_game = Game(plays_info[0], plays=plays_info[1])
         
         top_ten_by_total_time.append(time_game)
@@ -40,6 +48,6 @@ def index(request):
     
     postprocessed_status['top_ten_by_total_time'] = top_ten_by_total_time
     postprocessed_status['top_ten_by_plays'] = top_ten_by_plays
-    postprocessed_status['total_time'] = status['total_time']
+    postprocessed_status['total_time'] = readable_time(status['total_time'])
     
     return render_to_response("status/index.html",postprocessed_status)
