64a204c73b581c1a4d28a7686c2c22b92404838b
Alex Rasmussen <alexras@acm.org>
2011-01-18 17:34:01 -0800

    Making paths relative to /home/arcade so that the server can be run by the user 'arcade' rather than my sudo-capable username.

--- WebStatus/settings.py
+++ WebStatus/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/alexras/src/cse-arcade/WebStatus/WebStatus.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/arcade/src/cse-arcade/WebStatus/WebStatus.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -85,7 +85,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/alexras/src/cse-arcade/WebStatus/templates",
+    "/home/arcade/src/cse-arcade/WebStatus/templates",
 )
 
 INSTALLED_APPS = (
